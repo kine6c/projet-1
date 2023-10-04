888f980fef5ca783593ce27aa553f58db2c5c325
kine6ct <aurokine6c@gmail.com>
2023-10-04 18:29:38 +0000

    premiere tache

---
+++ examen_devops.py
@@ -0,0 +1,6 @@
+#creer un generateur de mot de passe
+
+#je demande a l'utilisateur de donnez le nombre de mot de passe que vous souhaitez reçevoir
+nbr_de_mdp = int(input("Donnez le nombre de mot de passe que vous souhaitez reçevoir : "))
+
+
